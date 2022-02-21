4dca708fe382213bfdaf8759eb36ad0b87dc28b5
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-02-21 16:23:40 +0100

    bugFix

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,15 +17,26 @@ function trennen() {
         output.innerHTML = str.slice(0, userInputP);
         output2.innerHTML = str.slice(userInputP);
     }
-    else if (document.getElementById("after").checked === true) {
-        console.log("if test2")
-        output.innerHTML = str.slice(0, userInputP + position.length);
-        output2.innerHTML = str.slice(userInputP + position.length);
-    }
-    else {
+    else if (userInputP == -1) {
         console.log("error!")
         output.innerHTML = `Das Zeichen konnte nicht gefunden werden`;
         output2.innerHTML = str;
     }
+    else {
+        console.log("if test2")
+        output.innerHTML = str.slice(0, userInputP + position.length);
+        output2.innerHTML = str.slice(userInputP + position.length);
+    }
+    // else if (document.getElementById("after").checked === true) {
+    //     console.log("if test2")
+    //     output.innerHTML = str.slice(0, userInputP + position.length);
+    //     output2.innerHTML = str.slice(userInputP + position.length);
+    // }
+    // else {
+    //     console.log("error!")
+    //     output.innerHTML = `Das Zeichen konnte nicht gefunden werden`;
+    //     output2.innerHTML = str;
+    // }
+
 
 }
